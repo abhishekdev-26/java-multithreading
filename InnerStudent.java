64ee6a5d9fa6b7ff7b9a4e8b905c4e5f4ea57559
Abhishek Pal <abhishekpal261127@gmail.com>
2026-09-02 15:04:54 +0530

    Dinesh's fixing bugs

--- InnerStudent.java
+++ InnerStudent.java
@@ -1,66 +1,57 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 class Amit implements Runnable {
-
     @Override
-    public void run(){
+    public void run() {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(10);
 
-        for(int ans : arr){
-           try{
-            Thread.sleep(1000);
-               System.out.println(ans);
-           } catch(Exception ex){
-            ex.printStackTrace();
-           } 
+        for (int ans : arr) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("[Amit] " + ans);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
-
     }
-    
 }
 
-
 class Dinesh implements Runnable {
-
     @Override
-    public void run(){
+    public void run() {
         ArrayList<String> dr = new ArrayList<>();
         dr.add("a");
         dr.add("b");
         dr.add("c");
         dr.add("d");
-        for(String s : dr){
-            try{
+
+        for (String s : dr) {
+            try {
                 Thread.sleep(1000);
-                System.out.println(s);
-            } catch(Exception ex){
+                System.out.println("[Dinesh] " + s);
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
-
 }
 
 class Abhishek implements Runnable {
-
     @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Abhishek Thread Count: " + i);
+            System.out.println("[Abhishek] Count: " + i);
             try {
-               Thread.sleep(1000);
-               // Removed the duplicate println from here so it counts cleanly
+                Thread.sleep(1000);
             } catch (Exception ex) {
-               ex.printStackTrace();
+                ex.printStackTrace();
             }
         }
     }
 }
 
-// File must be named InnerStudent.java because this class is public
 public class InnerStudent implements Runnable {
 
     @Override
@@ -75,67 +66,85 @@ public class InnerStudent implements Runnable {
         for (String ans : arr) {
             try {
                 Thread.sleep(3000);
-                System.out.println(ans);
+                System.out.println("[InnerStudent Part 1] " + ans);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
 
+        // Fixed: Adding elements to arr2 instead of arr
         ArrayList<String> arr2 = new ArrayList<>();
-        arr.add("Abhishek");
-        arr.add("Bhai");
-        
-        for(String name : arr2){
-            try{
+        arr2.add("Abhishek");
+        arr2.add("Bhai");
+
+        for (String name : arr2) {
+            try {
                 Thread.sleep(2000);
-               System.out.println(name);
-            } catch(Exception e){
-              e.printStackTrace();
+                System.out.println("[InnerStudent Part 2] " + name);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-
     }
-    
-    
-    // Main method added so you can run the file directly
+
     public static void main(String[] args) {
-        System.out.println("--- Starting Main Thread ---");
-
-        // Thread t1 = new Thread(new Abhishek());
-        // Thread t2 = new Thread(new InnerStudent());
-
-        // t1.start();
-        // t2.start();
-
-        // Thread t3 = new Thread(new InnerStudent());
-        // t3.start();
-
         Scanner sc = new Scanner(System.in);
         int choice;
-        
-        do{
-            System.out.println("1. sgsh");
-            System.out.println("2. xsaas");
-            System.out.println("3. shxva");
-            System.out.println("4. shv");
-            System.out.println("Enter your choice:");
+
+        System.out.println("=== Multi-Threading Demo ===");
+
+        do {
+            System.out.println("\nSelect a Thread Task to Run:");
+            System.out.println("1. Run InnerStudent Task");
+            System.out.println("2. Run Abhishek Task");
+            System.out.println("3. Run Dinesh Task");
+            System.out.println("4. Run Amit Task");
+            // System.out.println("5. Run Future Task"); // Add menu item here for future cases
+            System.out.println("0. Exit Application");
+            System.out.print("Enter your choice: ");
+            
             choice = sc.nextInt();
+
             switch (choice) {
                 case 1:
-                    Thread t2 = new Thread(new InnerStudent());
-                    t2.start();
+                    System.out.println("--> Starting InnerStudent Thread...");
+                    new Thread(new InnerStudent()).start();
                     break;
-                case 2:
-                    Thread t3 = new Thread();
-                    t3.start();  
-                    break;  
-                default:
-                    System.out.println("Please Enter a valid number");  
-                    break;  
 
+                case 2:
+                    System.out.println("--> Starting Abhishek Thread...");
+                    // Fixed: Added target Runnable object to Thread
+                    new Thread(new Abhishek()).start();
+                    break;
+
+                case 3:
+                    System.out.println("--> Starting Dinesh Thread...");
+                    new Thread(new Dinesh()).start();
+                    break;
+
+                case 4:
+                    System.out.println("--> Starting Amit Thread...");
+                    new Thread(new Amit()).start();
+                    break;
+
+                /* TEMPLATE FOR FUTURE CASES:
+                case 5:
+                    System.out.println("--> Starting Future Thread...");
+                    new Thread(new YourFutureRunnableClass()).start();
+                    break;
+                */
+
+                case 0:
+                    System.out.println("Exiting application...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+                    break;
             }
 
-        } while(choice != 1);
+        } while (choice != 0);
 
+        sc.close();
     }
 }
